3c40d6cc130d6d866ac40527cc60c15120055d36
mkotov <m.kotau@mail.ru>
2019-12-04 12:11:18 +0300

    add allerts

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import {Home} from './pages/Home'
 import {AddApp} from './pages/AddApp'
 import {Navbar} from './components/Navbar'
 import {AllApps} from './pages/AllApps'
+import {Alert} from './components/Alert'
+import {AlertState} from './context/alert/AlertState'
 
 function App() {
   return (
+    <AlertState>
     <BrowserRouter>
       <Navbar />
-    <div className="container pt-4">
-      <Switch>
-         <Route path={'/'} exact component ={Home}/>
-         <Route path={'/addApp'} exact component ={AddApp}/>
-         <Route path={'/allApps'} exact component ={AllApps}/>
-       </Switch>
-    </div>
+      <div className="container pt-4">
+        <Alert />
+        <Switch>
+          <Route path={'/'} exact component ={Home}/>
+          <Route path={'/addApp'} exact component ={AddApp}/>
+          <Route path={'/allApps'} exact component ={AllApps}/>
+        </Switch>
+      </div>
     </BrowserRouter>
+    </AlertState>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import {AlertContext} from '../context/alert/alertContext'
 
 export const Form = () => {
+const [value, setValue] = useState('')
+const alert = useContext(AlertContext)
+
+const submitHandler = event => {
+    event.preventDefault()
+
+    if(value.trim()) {
+      //...
+      alert.show('Application has been added', 'success')
+      setValue('')
+    }else {
+      alert.show('aap\'s url can\'t be empty ')
+    }
+
+
+
+}
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
         type="text"
         className="form-control"
         placeholder="enter new app's url here"
+        value={value}
+        onChange={e => setValue(e.target.value)}
         />
       </div>
     </form>
